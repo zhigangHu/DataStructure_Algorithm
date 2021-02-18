1c3a48fa3c67b1a5fd3f10c7695e9253125eb0d1
zhigangHu <comqiao@gmail.com>
2021-02-18 01:17:28 -0700

    Update InsertionSort.java

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -6,10 +6,19 @@ public class InsertionSort<E extends Comparable<E>> extends Sorting{
     protected void sort() {
         for(int begin=1;begin< array.length;begin++){
             int cur=begin;
-            while(cur>0&&compare(cur,cur-1)<0){
-                swap(cur,cur-1);
-                cur--;
+            //Optimize
+            E v=array[cur]
+////        Origin
+////            while(cur>0&&compare(cur,cur-1)<0){
+//                swap(cur,cur-1);
+//                cur--;
+//            }
+//           Optimize
+            while(cur>0 && compare(v,array[cur-1])<0){
+                array[cur]=array[cur-1];
+                cur--
             }
+            array[cur]=v;
         }
     }
 }
